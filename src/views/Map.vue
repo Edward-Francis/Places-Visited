762add605159cbefa06b0b5d71c593feb1a743ef
Edward Francis <edwardafrancis@hotmail.com>
2019-09-12 16:53:13 +0100

    added listPlacesQuery to table

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,13 +1,27 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="places"
-    :items-per-page="5"
-    class="elevation-1"
-  ></v-data-table>
+  <div class="home">
+    <amplify-connect :query="listPlacesQuery">
+      <template slot-scope="{ loading, data, errors }">
+        <div v-if="loading">Loading...</div>
+
+        <div v-else-if="errors.length > 0">{{ errors }}</div>
+
+        <div v-else-if="data">
+          <v-data-table
+            :headers="headers"
+            :items="data.listPlaces.items"
+            :items-per-page="5"
+            class="elevation-1"
+          ></v-data-table>
+        </div>
+      </template>
+    </amplify-connect>
+  </div>
 </template>
 
-<script lang="ts">
+<script>
+import { listPlaces } from "@/graphql/queries";
+
 export default {
   data() {
     return {
@@ -16,28 +30,13 @@ export default {
         { text: "Name", value: "name" },
         { text: "Latitude", value: "latitude" },
         { text: "Longitude", value: "longitude" }
-      ],
-      places: [
-        {
-          id: "1bbb8301-d030-4e65-b30a-5a6321afd69b",
-          name: "London",
-          latitude: 51.5074,
-          longitude: 0.1278
-        },
-        {
-          id: "5747bba2-b531-4419-8e0e-4eebfabc8a91",
-          name: "Paris",
-          latitude: 48.8566,
-          longitude: 2.3522
-        },
-        {
-          id: "c4357ef8-15bc-4215-a82e-75df2142a4bd",
-          name: "Rome",
-          latitude: 41.9028,
-          longitude: 12.4964
-        }
       ]
     };
+  },
+  computed: {
+    listPlacesQuery() {
+      return this.$Amplify.graphqlOperation(listPlaces);
+    }
   }
 };
 </script>

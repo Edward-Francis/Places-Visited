f62876ef476dd683898ab6c2318c290d6eae3dc8
Edward Francis <edwardafrancis@hotmail.com>
2019-09-12 18:04:23 +0100

    updated to subscription

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
-    <amplify-connect :query="listPlacesQuery">
+    <amplify-connect
+      :query="listPlacesQuery"
+      :subscription="createTodoSubscription"
+      :onSubscriptionMsg="onCreateTodo"
+    >
       <template slot-scope="{ loading, data, errors }">
         <div v-if="loading">Loading...</div>
 
@@ -10,7 +14,7 @@
           <v-data-table
             :headers="headers"
             :items="data.listPlaces.items"
-            :items-per-page="5"
+            :items-per-page="100"
             class="elevation-1"
           ></v-data-table>
         </div>
@@ -23,6 +27,7 @@
 
 <script>
 import { listPlaces } from "@/graphql/queries";
+import { onCreatePlace } from "@/graphql/subscriptions";
 import CreatePlace from "@/components/CreatePlace";
 
 export default {
@@ -39,10 +44,22 @@ export default {
   computed: {
     listPlacesQuery() {
       return this.$Amplify.graphqlOperation(listPlaces);
+    },
+    createTodoSubscription() {
+      return this.$Amplify.graphqlOperation(onCreatePlace);
     }
   },
   components: {
     CreatePlace
+  },
+  methods: {
+    onCreateTodo(prevData, newData) {
+      console.log("New todo from subscription...");
+      const newTodo = newData.onCreatePlace;
+      console.log(newData);
+      prevData.data.listPlaces.items.push(newTodo);
+      return prevData.data;
+    }
   }
 };
 </script>
